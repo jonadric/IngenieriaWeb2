91c4a6f5940de671d42eec5c1129eb1eb774c1b2
jonadric <jonathanastroz@gmail.com>
2018-10-18 20:46:43 -0500

    adicion elementos

--- corte2/jsP/nodos.js
+++ corte2/jsP/nodos.js
@@ -1,13 +1,43 @@
+
 var ch;
+var select;
+var nodos [];
+// var coinImage = new Image();
+// coinImage.src = "img/coin-sprite-animation.png";
+
+// function sprite (options) {
+				
+//     var that = {};
+					
+//     that.context = options.context;
+//     that.width = options.width;
+//     that.height = options.height;
+//     that.image = options.image;
+
+//     return that;
+// }
 
 function preparar() {
-	ch = document.getElementsByTagName("p")
+	ch = document.getElementsByTagName("li")
 }
 
 function crear() {
-	var hijo = document.createElement("p");
-	var text = document.createTextNode("Hijo" + (ch.length + 1));
-	hijo.appendChild(text);
-	document.getElementById("tronco").appendChild(hijo);
+	var rama = document.crea teElement("li");
+	var contenido = document.createTextNode("rama" );
+	rama.appendChild(contenido);
+	
+	rama = document.getElementById("tronco").appendChild(rama);
+	// hoja.insertLater(contenido,rama);
+
 	preparar();
 }
+function eliminar() {
+	// var del.getElementById("li").remove();
+}
+function selector(){
+
+	select = document.getElementsByTagName("li")[0];
+}
+
+// $('button').on('click', function (e) {
+//     e.crear();
